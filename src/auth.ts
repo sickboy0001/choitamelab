6d62795604bcfde747820139a03f0144cbc92c7e
sickboy0001 <syunjyu0001@gmail.com>
2026-03-14 06:27:21 +0900

    fix github actions

--- src/auth.ts
+++ src/auth.ts
@@ -24,8 +24,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
     CredentialsProvider({
       name: "メールアドレス",
